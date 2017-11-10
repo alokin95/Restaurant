64646d821fa1824f6871184a1f76a44051bfa2bc
alokin95 <nikola_m95@hotmail.com>
2017-11-10 05:50:01 +0100

    Made minor changes

--- js/script.js
+++ js/script.js
@@ -35,9 +35,16 @@ function slider() {
 
 
 
+
+/* ZA FORMU DATUM*/
+let dateControl = document.querySelector('input[type="date"]');
+let date=new Date();
+/*KRAJ*/
+
+
 /*DISHES AJAX*/
 
-function request(url,func){
+function request(url,f){
 
 	let xhttp=new XMLHttpRequest();
 
@@ -47,7 +54,7 @@ function request(url,func){
 
 	xhttp.onreadystatechange=function(){
 		if (this.readyState === 4 && this.status===200){
-			func(this);
+			f(this);
 		}
 	}
 
@@ -55,14 +62,25 @@ function request(url,func){
 	xhttp.send();
 }
 
-function getDishes(xml){
-	let x=JSON.stringify(xml.responseText);
+function getDinner(xml){
+	let obj=JSON.parse(xml.responseText);
+	let ime=obj['DINNER MENU'][0].name;
+  populateDinner(ime);
+
 }
 
-
-document.getElementById("dugme").addEventListener("click",function(){
-
-	request("dishes.json",getDishes);
-
-});
+function populateDinner(name){
+  for (let i=0;i<5;i++){
+    var newDiv=document.createElement('div');
+    var newP=document.createElement('p');
+    var newTextNode=document.createTextNode(name);
+    newP.appendChild(newTextNode);
+    newDiv.setAttribute('class','recipe');
+    newDiv.appendChild(newP);
+    document.getElementById('dinner').appendChild(newDiv);
+  }
+}
+window.addEventListener('load', function(){
+  request("dinner.json", getDinner);
+})
 /*AJAX END*/
